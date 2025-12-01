a7da62cecea4b85e9e8569854758a6e0f93e129b
GauravGulati06 <gauravgulati619@gmail.com>
2025-12-01 13:54:49 +0530

    updated postgres url

--- app/database.py
+++ app/database.py
@@ -4,7 +4,13 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 import os
 load_dotenv()
-SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+USERNAME = os.getenv("USERNAME")
+HOST = os.getenv("HOST")
+PASSWORD = os.getenv("PASSWORD")
+DATABASE = os.getenv("DATABASE")
+PORT = os.getenv("PORT")
+# SQLALCHEMY_DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+SQLALCHEMY_DATABASE_URL = f"postgresql://{USERNAME}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
--- app/main.py
+++ app/main.py
@@ -2,8 +2,19 @@ from fastapi import FastAPI
 from . import models
 from .database import engine
 from .routers import posts, users, auth, vote
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
